8b0e11a817a6360913741b44d25391317a652dd8
Sergio Romano <sergiojavierromano@gmail.com>
2021-10-28 21:26:46 -0300

    Commit sergio 1: Clase Pato

---
+++ src/Entidad/Pato.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidad;
+
+/**
+ *
+ * @author Sergio
+ */
+public class Pato {
+    private String raza;
+    private String color;
+    private int edad;
+    
+}
